317fa3042e5ca9716c480f09e87edf062b9f26a8
pengmm <pengmm@allinpay.com>
2022-07-20 18:12:44 +0800

    rest

--- MyTest/src/main/java/com/ming/mytest/controller/TestController.java
+++ MyTest/src/main/java/com/ming/mytest/controller/TestController.java
@@ -2,6 +2,9 @@ package com.ming.mytest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * Created by ming.
@@ -9,9 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+	@Resource
+	RestTemplate restTemplate;
+	@Resource
+	private RestTemplate httpClientTemplate;
+
 	@GetMapping("/test")
 	public String test(){
-		System.out.println("源码环境构建成功...");
-		return "ok";
+		String forEntity = restTemplate.getForObject("https://zhuanlan.zhihu.com/api/columns/zhihuadmin", String.class);
+		return forEntity;
+	}
+
+	@GetMapping("/testa")
+	public String testA(){
+		String forEntity = httpClientTemplate.getForObject("https://zhuanlan.zhihu.com/api/columns/zhihuadmin", String.class);
+		return forEntity;
 	}
 }
